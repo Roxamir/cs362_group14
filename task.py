97e3f21ccfb0993522c7c7b5cb2160e2aca1651a
Roxamir <101136913+Roxamir@users.noreply.github.com>
2024-05-22 08:48:04 -0700

    set up skeleton code for task.py

---
+++ task.py
@@ -0,0 +1,23 @@
+def conv_num(num_str):
+    """
+    This function takes in a string and converts it into a base 10 number
+    and returns it. Returns None if string is an invalid format.
+    """
+    pass
+
+
+def my_datetime(num_sec):
+    """
+    This function takes an integer value that represents the number of seconds
+    since the epoch: January 1st, 1970. Takes 'num_sec', converts to date and
+    returns it as a string with the format MM-DD-YYYY.
+    """
+    pass
+
+
+def conv_endian(num, endian='big'):
+    """
+    This function takes in an integer value as 'num' and conferts it to a
+    hexadecimal number. Endian type is determined by the flag 'endian'.
+    """
+    pass
