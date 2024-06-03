0aadee6cfa3c4d7e45d4476aa2eecae11a230db9
sahilg2000 <86344289+sahilg2000@users.noreply.github.com>
2024-06-03 17:38:55 +0530

    Commit: Updated 'tests.py' to test 'my_datetime'
    'tests.py' now has additional tests for 'my_datetime'
    Function is working as expected.
    
    Signed-off-by: sahilg2000 <86344289+sahilg2000@users.noreply.github.com>

--- tests.py
+++ tests.py
@@ -5,6 +5,7 @@ import random
 
 class TestCase(unittest.TestCase):
     # test conv_num
+    # function 1
     def test_conv_num_decimal_pos(self):
         """Test positive decimal numbers."""
         self.assertTrue(True)
@@ -26,11 +27,25 @@ class TestCase(unittest.TestCase):
         self.assertTrue(True)
 
     # test my_datetime
-    def test_my_datetime(self):
+    # function 2
+    def test_my_datetime_1(self):
+        """Tests my_datetime function."""
+        self.assertEqual(task.my_datetime(123456789), '11-29-1973')
+
+    def test_my_datetime_2(self):
+        """Tests my_datetime function."""
+        self.assertEqual(task.my_datetime(9876543210), '12-22-2282')
+
+    def test_my_datetime_3(self):
+        """Tests my_datetime function."""
+        self.assertEqual(task.my_datetime(201653971200), '02-29-8360')
+
+    def test_my_datetime_base(self):
         """Tests my_datetime function."""
         self.assertEqual(task.my_datetime(0), '01-01-1970')
 
     # test conv_endian
+    # function 3
     def test_conv_endian_little_pos(self):
         """Tests conv_endian function for positive little endian."""
         self.assertEqual('32 6B', task.conv_endian(27442, 'little'))
