65fc88927ba7feab9a8362b5dc8f1aed53ace058
Roxamir <101136913+Roxamir@users.noreply.github.com>
2024-06-03 14:56:13 -0700

    Fix Issue with decimal check and int number conversion.

--- task.py
+++ task.py
@@ -13,7 +13,7 @@ def conv_num(num_str):
     # negative check
     if num_str[0] == '-':
         negative_check = True
-        # remove negatige from string
+        # remove negative from string
         num_str = num_str[1:]
     else:
         negative_check = False
@@ -22,7 +22,7 @@ def conv_num(num_str):
     decimal_count = 0
 
     for i in range(num_length):
-        # demical check
+        # decimal check
         if num_str[i] == ".":
             decimal_count += 1
 
@@ -34,7 +34,7 @@ def conv_num(num_str):
         # check if hexadecimal section is empty
         if num_length == 2:
             return None
-
+        hex_num = 0
         # omit first two characters of hexadecimal string
         hexa_str = num_str[2:].lower()
         exp = 0
@@ -44,48 +44,50 @@ def conv_num(num_str):
         for char in reversed(hexa_str):
             if '0' <= char <= '9':
                 # ord(0) = 48. subtract 48 to get int value
-                decimal_num += (ord(char) - 48) * (16 ** exp)
+                hex_num += (ord(char) - 48) * (16 ** exp)
 
             elif 'a' <= char <= 'f':
                 # ord('a') = 97, subtract 87 to get hexadecimal values
-                decimal_num += (ord(char) - 87) * (16 ** exp)
+                hex_num += (ord(char) - 87) * (16 ** exp)
 
             else:
                 return None
-            # increase 16 exponeent for next hexadecimal position
+            # increase 16 exponent for next hexadecimal position
             exp += 1
+        return -hex_num if negative_check else hex_num
 
     # decimal conversion
     # multipliers to move to different base-10 digits
     tens_multiplier = 1
     tenths_multiplier = 0.1
 
-    if decimal_count == 1:
-
-        # split string into whole number and fractional number str
-        whole_num, fraction_num = num_str.split('.')
-
+    if decimal_count >= 0:
+        if decimal_count == 1:
+            # split string into whole number and fractional number str
+            whole_num, fraction_num = num_str.split('.')
+        else:
+            whole_num = num_str
         # iterate through whole_num backwards to ascend through base-10 digits
         for char in reversed(whole_num):
-            if char >= '0' and char <= '9':
-                decimal_num += (ord(char) - 48) * tens_multiplier
+            if char < '0' or char > '9':
+                return None
 
             else:
-                return None
+                decimal_num += (ord(char) - 48) * tens_multiplier
 
             # move to next base-10 position
             tens_multiplier *= 10
+        if decimal_count != 0:
+            # iterate through fraction_num to descend through base-10 digits
+            for char in fraction_num:
+                if '0' <= char <= '9':
+                    decimal_num += (ord(char) - 48) * tenths_multiplier
 
-        # iterate through fraction_num to descend through base-10 digits
-        for char in fraction_num:
-            if char >= '0' and char <= '9':
-                decimal_num += (ord(char) - 48) * tenths_multiplier
+                else:
+                    return None
 
-            else:
-                return None
-
-            # move to next base-10 position
-            tenths_multiplier *= 0.1
+                # move to next base-10 position
+                tenths_multiplier *= 0.1
 
     return -decimal_num if negative_check else decimal_num
 
@@ -105,3 +107,16 @@ def conv_endian(num, endian='big'):
     hexadecimal number. Endian type is determined by the flag 'endian'.
     """
     pass
+
+
+if __name__ == "__main__":
+
+    print(conv_num('12345'))       # returns 12345
+    print(conv_num('-123.45'))     # returns -123.45
+    print(conv_num('.45'))         # returns 0.45
+    print(conv_num('123.'))        # returns 123.0
+    print(conv_num('0xAD4'))       # returns 2772
+    print(conv_num('0Xad4'))       # returns 2772
+    print(conv_num('0xAZ4'))       # returns None
+    print(conv_num('12345A'))      # returns None
+    print(conv_num('12.3.45'))     # returns None
