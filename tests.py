293446a3133952fe3a5a496683082a566628d02f
sahilg2000 <86344289+sahilg2000@users.noreply.github.com>
2024-06-04 00:09:52 +0530

    Commit: Removed functions 'conv_num' and 'my_datetime'
    Removed functions to complete pull request and avoid merge conflict.
    
    Signed-off-by: sahilg2000 <86344289+sahilg2000@users.noreply.github.com>

--- tests.py
+++ tests.py
@@ -4,45 +4,6 @@ import random
 
 
 class TestCase(unittest.TestCase):
-    # test conv_num
-    # function 1
-    def test_conv_num_decimal_pos(self):
-        """Test positive decimal numbers."""
-        self.assertTrue(True)
-
-    def test_conv_num_hex_pos(self):
-        """Test positive hexadecimal numbers."""
-        self.assertTrue(True)
-
-    def test_conv_num_decimal_neg(self):
-        """Test negative decimal numbers."""
-        self.assertTrue(True)
-
-    def test_conv_num_decimal_point_pos(self):
-        """Test positive numbers with decimal point."""
-        self.assertTrue(True)
-
-    def test_conv_num_decimal_point_neg(self):
-        """Test negative numbers with decimal point."""
-        self.assertTrue(True)
-
-    # test my_datetime
-    # function 2
-    def test_my_datetime_1(self):
-        """Tests my_datetime function."""
-        self.assertEqual(task.my_datetime(123456789), '11-29-1973')
-
-    def test_my_datetime_2(self):
-        """Tests my_datetime function."""
-        self.assertEqual(task.my_datetime(9876543210), '12-22-2282')
-
-    def test_my_datetime_3(self):
-        """Tests my_datetime function."""
-        self.assertEqual(task.my_datetime(201653971200), '02-29-8360')
-
-    def test_my_datetime_base(self):
-        """Tests my_datetime function."""
-        self.assertEqual(task.my_datetime(0), '01-01-1970')
 
     # test conv_endian
     # function 3
