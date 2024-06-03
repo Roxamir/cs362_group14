462e53b24ef7b867753f335268d16647fd019c21
sahilg2000 <86344289+sahilg2000@users.noreply.github.com>
2024-06-03 17:03:07 +0530

    Commit: Updated tests.py for conv_endian
    'tests.py': Added random positive and negative tests for conv_endian to check using random endian numbers
    
    Signed-off-by: sahilg2000 <86344289+sahilg2000@users.noreply.github.com>

--- tests.py
+++ tests.py
@@ -1,8 +1,10 @@
 import unittest
 import task
+import random
 
 
 class TestCase(unittest.TestCase):
+    # test conv_num
     def test_conv_num_decimal_pos(self):
         """Test positive decimal numbers."""
         self.assertTrue(True)
@@ -23,10 +25,12 @@ class TestCase(unittest.TestCase):
         """Test negative numbers with decimal point."""
         self.assertTrue(True)
 
+    # test my_datetime
     def test_my_datetime(self):
         """Tests my_datetime function."""
         self.assertEqual(task.my_datetime(0), '01-01-1970')
 
+    # test conv_endian
     def test_conv_endian_little_pos(self):
         """Tests conv_endian function for positive little endian."""
         self.assertEqual('32 6B', task.conv_endian(27442, 'little'))
@@ -42,6 +46,48 @@ class TestCase(unittest.TestCase):
     def test_conv_endian_big_neg(self):
         """Tests conv_endian function for negative big endian."""
         self.assertEqual('-16 61', task.conv_endian(-5729, 'big'))
+
+    def test_conv_endian_random_pos(self):
+        """Tests conv_endian function for random positive values."""
+        endian = random.choice(['little', 'big'])
+        num = random.randrange(0, 1000000)  # negative million to positive million, random number
+        hex_str = str(hex(num)).upper()[2:]
+        hex_list = []
+        if len(hex_str) % 2:
+            hex_str = '0' + hex_str
+        byte = ''
+        for index in range(len(hex_str)):
+            byte = byte + hex_str[index]
+            if index % 2:
+                hex_list.append(byte)
+                byte = ''
+        if endian == 'little':
+            hex_num = ' '.join(map(str, hex_list[::-1]))
+        else:
+            hex_num = ' '.join(map(str, hex_list))
+
+        self.assertEqual(hex_num, task.conv_endian(num, endian))
+
+    def test_conv_endian_random_neg(self):
+        """Tests conv_endian function for random positive values."""
+        endian = random.choice(['little', 'big'])
+        num = random.randrange(-1000000, -1)          # negative million to positive million, random number
+        hex_str = str(hex(num)).upper()[3:]
+        hex_list = []
+        if len(hex_str) % 2:
+            hex_str = '0' + hex_str
+        byte = ''
+        for index in range(len(hex_str)):
+            byte = byte + hex_str[index]
+            if index % 2:
+                hex_list.append(byte)
+                byte = ''
+        if endian == 'little':
+            hex_num = ' '.join(map(str, hex_list[::-1]))
+        else:
+            hex_num = ' '.join(map(str, hex_list))
+        hex_num = '-' + hex_num
+        self.assertEqual(hex_num, task.conv_endian(num, endian))
 
 
 if __name__ == '__main__':
