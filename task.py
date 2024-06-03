e04653f98cd953c81e2a4ce38c2a14a83fd72b9b
sahilg2000 <86344289+sahilg2000@users.noreply.github.com>
2024-06-03 15:13:11 +0530

    Commit 1: Basic Tests and Empty Funtions
    Commit includes changes to 'task.py' and 'tests.py'.
    'task.py' now returns some values and 'tests.py' validates those values by assertion.
    
    Signed-off-by: sahilg2000 <86344289+sahilg2000@users.noreply.github.com>

--- task.py
+++ task.py
@@ -3,7 +3,7 @@ def conv_num(num_str):
     This function takes in a string and converts it into a base 10 number
     and returns it. Returns None if string is an invalid format.
     """
-    pass
+    return True
 
 
 def my_datetime(num_sec):
@@ -12,12 +12,12 @@ def my_datetime(num_sec):
     since the epoch: January 1st, 1970. Takes 'num_sec', converts to date and
     returns it as a string with the format MM-DD-YYYY.
     """
-    pass
+    return '01-01-1970'
 
 
 def conv_endian(num, endian='big'):
     """
-    This function takes in an integer value as 'num' and conferts it to a
+    This function takes in an integer value as 'num' and converts it to a
     hexadecimal number. Endian type is determined by the flag 'endian'.
     """
-    pass
+    return '0E 91 A2'
--- tests.py
+++ tests.py
@@ -1,10 +1,35 @@
 import unittest
+import task
 
 
 class TestCase(unittest.TestCase):
-
-    def test1(self):
+    def test_conv_num_decimal_pos(self):
+        """Test positive decimal numbers."""
         self.assertTrue(True)
+
+    def test_conv_num_hex_pos(self):
+        """Test positive hexadecimal numbers."""
+        self.assertTrue(True)
+
+    def test_conv_num_decimal_neg(self):
+        """Test negative decimal numbers."""
+        self.assertTrue(True)
+
+    def test_conv_num_decimal_point_pos(self):
+        """Test positive numbers with decimal point."""
+        self.assertTrue(True)
+
+    def test_conv_num_decimal_point_neg(self):
+        """Test negative numbers with decimal point."""
+        self.assertTrue(True)
+
+    def test_my_datetime(self):
+        """Tests my_datetime function."""
+        self.assertEqual(task.my_datetime(0), '01-01-1970')
+
+    def test_conv_endian(self):
+        """Tests conv_endian function."""
+        self.assertEqual(task.conv_endian(954786), '0E 91 A2')
 
 
 if __name__ == '__main__':
